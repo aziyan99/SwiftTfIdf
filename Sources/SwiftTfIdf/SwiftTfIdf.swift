53f1655d9f74ccd06275e89a32f857034bd26e56
aziyan99 <rajaazian08@gmail.com>
2022-01-23 21:40:08 +0700

    add some comments, hopefully not forget how the code woks

--- Sources/SwiftTfIdf/SwiftTfIdf.swift
+++ Sources/SwiftTfIdf/SwiftTfIdf.swift
@@ -2,18 +2,22 @@ import NaturalLanguage
 import Foundation
 
 public class SwiftTfIdf {
-    public var rawSentences = [String]()
-    public var topN: Int
-    public var stopWords: [String]
+    public var rawSentences = [String]() // raw sentences
+    public var topN: Int // how many top word want to get
+    public var stopWords: [String] // the stop word :)
     
     public init(text: String, stopWords: [String], topN: Int) {
         self.stopWords = stopWords
         self.topN = topN
+        
+        // just another normal looping to get get the rawsentences from rawraw text (paradox heh :v)
         text.enumerateSubstrings(in: text.startIndex..., options: [.localized, .bySentences]) { (tag, _, _, _) in
             self.rawSentences.append(tag?.lowercased() ?? "")
         }
     }
     
+    
+    // Convert a bunch of sentences to array of words
     func sentences2ArrayOfWords(sentences: [String]) -> [[String]]{
         var arrayOfWordInSentences = [[String]]()
 
@@ -26,22 +30,13 @@ public class SwiftTfIdf {
             tokenizer.enumerateTokens(in: sentence.startIndex..<sentence.endIndex) { tokenRange, _ in
                 let word = String(sentence[tokenRange])
                 
-//                let syncQueue2 = DispatchQueue(label: "level2")
-                
+                // check if the word is stopword
                 let isStopWord = stopWords.contains(word)
                 
                 if !isStopWord {
                     words.append(word)
                     
                 }
-
-//                for stopWord in self.stopWords {
-//                    if stopWord != word {
-//                        syncQueue2.sync {
-//                            words.append(word)
-//                        }
-//                    }
-//                }
                 return true
             }
 
@@ -52,6 +47,7 @@ public class SwiftTfIdf {
         return arrayOfWordInSentences
     }
     
+    // calc the TF value
     func calcTf(document: [String]) -> [String: Float] {
         var TF_dict = [String: Float]()
 
@@ -71,6 +67,7 @@ public class SwiftTfIdf {
         return TF_dict
     }
 
+    // calc the DF value
     func calcDF(tfDict: [[String: Float]]) -> [String: Float] {
         var countDF = [String: Float]()
         for document in tfDict {
@@ -87,6 +84,7 @@ public class SwiftTfIdf {
         return countDF
     }
 
+    // calc the IDF value
     func calcIDF(nDocument: Int, df: [String: Float]) -> [String: Float] {
         var idfDict = [String: Float]()
         for term in df {
@@ -95,6 +93,7 @@ public class SwiftTfIdf {
         return idfDict
     }
     
+    // calc the TFIDF value
     func calcTfIdf(TF: [String: Float], IDF: [String: Float]) -> [String: Float] {
         var tfIdfDict = [String: Float]()
         for term in TF {
@@ -103,7 +102,7 @@ public class SwiftTfIdf {
         return tfIdfDict
     }
 
-
+    // calc the TFIDF vector
     func calcTFIDFVec(tfIdfDict: [String: Float], uniqueTerm: [String]) -> [Float] {
         var tfIdfVec = [Float]()
         for _ in 1...uniqueTerm.count {
@@ -121,26 +120,36 @@ public class SwiftTfIdf {
         return tfIdfVec
     }
     
+    // final calculation to get the tf, df, idf and tfidf value
     public func finalCount() -> [(key: String, value: Float)] {
+        
+        // convert raw sentences to array of sentences
         let dictOfWordInSentences = sentences2ArrayOfWords(sentences: rawSentences)
+        
+        // calc the tf value save it as dictionary
         var dictOfTF = [[String: Float]]()
         let nDocument = dictOfWordInSentences.count
-        var initValue = 0
         
+        // looping for calc the tf value
+        var initValue = 0
         for document in dictOfWordInSentences {
             let tf_dict = calcTf(document: document)
             dictOfTF.append(tf_dict)
         }
 
+        // calc the df value
         let dictOfDf = calcDF(tfDict: dictOfTF)
         _ = calcIDF(nDocument: nDocument, df: dictOfDf)
 
+        // initial dictionary of tf idf value
         var dictOfTFIDF = [[String: Float]]()
 
+        // calc the tf idf value
         for document in dictOfTF {
             dictOfTFIDF.append(calcTfIdf(TF: document, IDF: dictOfDf))
         }
 
+        // sorting the results
         let sortedDF = dictOfDf.sorted {
             return $0.value > $1.value
         }
@@ -156,7 +165,7 @@ public class SwiftTfIdf {
             initValue += 1
         }
 
-        //vector
+        // calc the tfidf vector
         var tfIdfVec = [[String: [Float]]]()
         initValue = 0
         for documents in dictOfTFIDF {
@@ -168,6 +177,7 @@ public class SwiftTfIdf {
             initValue += 1
         }
 
+        // ranking the results
         var rawRanking = [String: Float]()
         for terms in tfIdfVec {
             var rankingValue: Float = Float(0.0)
@@ -179,8 +189,7 @@ public class SwiftTfIdf {
             }
         }
 
-
-
+        // combine the ranking and its value
         var ranking = [String: Float]()
         for i in uniqueTerm {
             let isKeyExist = rawRanking[i] != nil
@@ -189,6 +198,7 @@ public class SwiftTfIdf {
             }
         }
 
+        // again.. just another sorting
         let sortedRanking = ranking.sorted {
             return $0.value > $1.value
         }
